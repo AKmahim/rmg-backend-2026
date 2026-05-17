d4ce4e28941134616330d6cfc4109520d94192c4
AKmahim <mdmahimhossen134@gmail.com>
2026-05-17 11:57:17 +0600

    add QuizController, Quiz model, and migration for quizzes table with necessary fields

---
+++ app/Models/quiz.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class quiz extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'phone_number',// unique phone number
+        'score',
+        'ip_address',
+        'user_agent',// mobile or desktop
+        'played_count', // number of times the user has played
+    ];
+}
